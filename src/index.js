934fd6824af19e0b8a30d13fd9bb33addcd86954
Natalia Repkina <pn.88@mail.ru>
2023-08-09 13:02:53 +0300

    fix: sort

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ module.exports = function towelSort(matrix) {
         return [];
     } else {
         return matrix.reduce((array, item, i) => {
-            item.sort((a, b) => a - b).map((e) => array.push(e));
+            item.sort((a, b) => (!(i & 1) ? a - b : b - a)).map((e) =>
+                array.push(e)
+            );
             return array;
         }, []);
     }
